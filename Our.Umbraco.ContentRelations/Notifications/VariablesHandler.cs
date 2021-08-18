0e341855a525fd6d177f36ebb753b731424df3da
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-08-18 15:10:49 +0200

    Add VariablesHandler

---
+++ Our.Umbraco.ContentRelations/Notifications/VariablesHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Routing;
+using Our.Umbraco.ContentRelations.Static;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Notifications;
+
+namespace Our.Umbraco.ContentRelations.Notifications
+{
+    public class VariablesHandler : INotificationHandler<ServerVariablesParsingNotification>
+    {
+        private readonly LinkGenerator _linkGenerator;
+
+        public VariablesHandler(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
+        public void Handle(ServerVariablesParsingNotification notification)
+        {
+            notification.ServerVariables.Add(Constants.Package.Alias, new Dictionary<string, object>
+            {
+
+            });
+        }
+    }
+}
