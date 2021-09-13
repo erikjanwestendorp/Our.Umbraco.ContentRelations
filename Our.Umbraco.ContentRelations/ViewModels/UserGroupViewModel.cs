ad05c15f2da7c4bc693abaa0820ba4282a4c62ff
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-09-13 21:18:55 +0200

    Create UserGroup Controller
    Get users groups  from IUserService and map to UserGroupViewModel

---
+++ Our.Umbraco.ContentRelations/ViewModels/UserGroupViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Our.Umbraco.ContentRelations.ViewModels
+{
+    public class UserGroupViewModel
+    {
+        public string Alias { get; set; }
+        public string Name { get; set; }
+        
+    }
+}
