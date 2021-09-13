694494169904ac9780f4a7950417f7656e5d254c
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-09-13 21:01:23 +0200

    Create settings controller and user groups resource

---
+++ Our.Umbraco.ContentRelations.Site/App_Plugins/ContentRelations/backoffice/contentrelationssettings/settings.controller.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    'use strict';
+
+    function contentRelationsSettingsController($scope) {
+
+        var vm = this;
+
+        console.log("content relations settings");
+        vm.loading = true;
+
+        function init() {
+
+            vm.loading = false;
+        }
+
+        init();
+
+    }
+
+    angular.module('umbraco')
+        .controller('contentRelationsSettingsController', contentRelationsSettingsController);
+})();
