d57dd7c7ec3fc1170e7f951ebc171c8541c33265
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-09-30 21:30:41 +0200

    Save configuration

--- Our.Umbraco.ContentRelations/App_Plugins/ContentRelations/resources/user.groups.resource.js
+++ Our.Umbraco.ContentRelations/App_Plugins/ContentRelations/resources/user.groups.resource.js
@@ -4,7 +4,8 @@
     function contentRelationsUserGroupsResource($http, $q) {
         var service = {
             getAll: getAll,
-            getConfiguration: getConfiguration
+            getConfiguration: getConfiguration,
+            saveConfiguration: saveConfiguration
         };
 
         var baseUrl = Umbraco.Sys.ServerVariables.ourUmbracoContentRelations.userGroups;
@@ -27,6 +28,19 @@
 
         function getConfiguration() {
             return $http.get(baseUrl + "GetConfiguration").then(success, error);
+
+            function success(result) {
+                return result.data;
+            }
+
+            function error(result) {
+                return $q.reject(result);
+            }
+        }
+
+        function saveConfiguration(config) {
+
+            return $http.post(baseUrl + "SaveConfiguration", config).then(success, error);
 
             function success(result) {
                 return result.data;
